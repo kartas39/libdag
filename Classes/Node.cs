d72e091b4c885415885df5ba5dda07ee4bcfbe0e
Sergei Krasnov <kartas39@gmail.com>
2022-01-20 22:17:03 +0100

    wip

--- Classes/Node.cs
+++ Classes/Node.cs
@@ -2,35 +2,33 @@
 
 using System.Runtime.CompilerServices;
 
-namespace libdag
+namespace libdag;
+
+public class Node
 {
-    public class Node
+    public enum NodeStatus { Pending, Started, Completed, Failed, ChildFailed }
+    public bool IsRoot { get; private set; }
+    public string NodeId { get; set; }
+    public Dictionary<string, Node> ParentNodes = new();
+    public Dictionary<string, Node> ChildNodes = new();
+    public NodeStatus Status
     {
-        public enum NodeStatus { Pending, Started, Completed, Failed, ChildFailed }
-        public bool IsRoot { get; private set; }
-        public string NodeId { get; set; }
-        public Dictionary<string, Node> ParentNodes = new Dictionary<string, Node>();
-        public Dictionary<string, Node> ChildNodes = new Dictionary<string, Node>();
-        public NodeStatus Status
-        {
-            get;
-            [MethodImpl(MethodImplOptions.Synchronized)]
-            set;
-        } = NodeStatus.Pending;
+        get;
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        set;
+    } = NodeStatus.Pending;
 
-        public Node(string nodeId)
-        {
-            NodeId = nodeId;
-            IsRoot = true;
-        }
-
-        public void AddParent(Node parent)
-        {
-            IsRoot = false;
-            ParentNodes[parent.NodeId] = parent;
-            parent.ChildNodes[NodeId] = this;
-        }
-
-
+    public Node(string nodeId)
+    {
+        NodeId = nodeId;
+        IsRoot = true;
     }
+
+    public void AddParent(Node parent)
+    {
+        IsRoot = false;
+        ParentNodes[parent.NodeId] = parent;
+        parent.ChildNodes[NodeId] = this;
+    }
+
 }
